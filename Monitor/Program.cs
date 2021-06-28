558fffc12300d4b024d296022854a1387e0717dd
kostyamospan <kostya04042001@gmail.com>
2021-06-28 11:04:55 +0300

    serilog, file logging

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Database;
 using IndexerCore;
-using IndexerCore.Extensions;
 using IndexingCore.RpcProviders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,7 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Web3Tracer.Tracers.Geth;
-using Microsoft.Extensions.Logging.Console;
+using Serilog;
 
 namespace Monitor
 {
@@ -21,16 +20,18 @@ namespace Monitor
 
         static async Task Main(string[] args)
         {
+            if (args.Length < 2) throw new ArgumentException("You need to provide at least 2 arguments: network type and LoggerFilePath");
+
             var _config = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.Parent.FullName)
-               .AddJsonFile("appsettings.json", false)
-               .AddUserSecrets<Program>()
-               .Build();
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.Parent.FullName)
+                .AddJsonFile("appsettings.json", false)
+                .AddUserSecrets<Program>()
+                .Build();
 
 
             var conn = new ConnectionStringsHelperService(_config);
 
-            var getBlockIOPrivateKeys = _config["BlockioPrivateKeys"].Split(",").Select(s => s.Trim()).ToList();
+            var getBlockIOPrivateKeys = _config["GetBlockIOPrivateKeys"].Split(",").Select(s => s.Trim()).ToList();
 
             if (getBlockIOPrivateKeys.Count() == 0) throw new ArgumentException("You must provide at least one private key to use GetBlock rpc provider");
 
@@ -40,9 +41,22 @@ namespace Monitor
 
             var tracer = new GethWeb3Tracer(web3);
 
+            if (!File.Exists(args[1]))
+                File.Create(args[1]);
+
+
+            var Logger = new LoggerConfiguration()
+                                .Enrich.FromLogContext()
+                                .WriteTo.Console()
+                                .WriteTo.File(args[1])
+                                .CreateLogger();
+
 
             var serviceProvider = new ServiceCollection()
-                .AddLogging(config => config.ClearProviders().AddConsole().SetMinimumLevel(LogLevel.Trace))
+                .AddLogging(config =>
+                {
+                    config.AddSerilog(Logger, true);
+                })
                 .BuildServiceProvider();
 
             serviceProvider
