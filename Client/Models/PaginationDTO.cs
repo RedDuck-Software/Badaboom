6939b5c70c7eeb85b6af31b201790cb8d39c48d9
Alexandr Okushko <alexandr.okushko@redduck.io>
2021-07-06 13:17:04 +0300

    add blazor web assembly app

---
+++ Client/Models/PaginationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class PaginationDTO
+    {
+        public int Page { get; set; } = 1;
+        public int QuantityPerPage { get; set; } = 10;
+    }
+}
---
+++ Client/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class Transaction
+    {
+        public string TxnHash { get; set; }
+        public string Method { get; set; }
+        public uint Block { get; set; }
+        public uint Age { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Value { get; set; }
+        public decimal TxnFee { get; set; }
+    }
+}
