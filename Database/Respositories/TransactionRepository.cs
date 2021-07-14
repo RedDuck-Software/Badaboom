07849bebf2987dcb482c8aa3aff8ef1401cdc275
kostyamospan <kostya04042001@gmail.com>
2021-07-14 13:13:48 +0300

    get paginated calls

--- Database/Respositories/TransactionRepository.cs
+++ Database/Respositories/TransactionRepository.cs
@@ -57,14 +57,46 @@ namespace Database.Respositories
 
         }
 
-        public async Task<IEnumerable<Call>> GetCallsByAddressAndMethodAsync(string address, string methodId)
+        public async Task<IEnumerable<Call>> GetCallsByAddressAndMethodAsync(CallsPagination pagination)
         {
-            var sql =
-                "select * from Calls " +
-                "where To = @To and " +
-                "MethodId = @MethodId";
+            if (pagination != null)
+            {
+                if (!(pagination.BlockId != null || pagination.To != null))
+                    throw new System.ArgumentException("Invalid pagination parameters");
+            }
 
-            return await SqlConnection.QueryAsync<Call>(sql, new { ContractAddress = address, MethodId = methodId });
+
+            string method = pagination?.MethodId;
+            string to = pagination?.To;
+            string block = pagination?.BlockId?.ToString();
+
+            string blockWherePaginationQuery = block == null ? "" : $"t.BlockId={block} {(to == null ? "" : " and ")}";
+            string toWherePaginationQuery = to == null ? "" : $"c.To=convert(binary(20),'{to}',1) {(method == null ? "" : " and ")}";
+            string methodWherePaginationQuery = method == null ? "" : $"c.MethodId=convert(binary(4),'{method}',1) ";
+
+            var sql =
+                "select " +
+                    "c.CallId " +
+                    "convert(varchar(6), c.[TransactionHash], 1) as TransactionHash, " +
+                    "c.Error " +
+                    "c.Type " +
+                    "convert(varchar(44), c.[From], 1) as From, " +
+                    "convert(varchar(44), c.[To], 1) as To, " +
+                    "convert(varchar(10), c.[MethodId], 1) as MethodId, " +
+                    "convert(varchar(MAX), c.[Input], 1) as Input, " +
+
+                "from " +
+                    "Calls c " +
+                block == null ? "" :
+                ("inner join Transactions t " +
+                "on " +
+                    "c.TransactionHash=t.TransactionHash") +
+                "where " +
+                    blockWherePaginationQuery +
+                    toWherePaginationQuery +
+                    methodWherePaginationQuery + ";";
+
+            return await SqlConnection.QueryAsync<Call>(sql);
         }
     }
 }
