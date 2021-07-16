9a2e3b45dcf2aa61b6b4c0efdde4afab4738ecbe
kostyamospan <kostya04042001@gmail.com>
2021-07-16 11:57:45 +0300

    methods filtering template, Contract abi DTO

---
+++ Badaboom.Client/Shared/Filters/MethodsFiltering.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Badaboom.Client.Shared.Filters
+{
+    public partial class MethodsFilteringBase 
+    {
+        [Parameter]
+        public string ContractAbi { get; set; }
+
+        [Inject]
+        public HttpClient Http { get; set; }
+            
+        
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+    }
+}
---
+++ Badaboom.Core/Models/DTOs/ContractAbiDTO.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badaboom.Core.Models.DTOs
+{
+    public class Input
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("indexed")]
+        public bool? Indexed { get; set; }
+    }
+
+    public class Output
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class Method
+    {
+        [JsonProperty("constant")]
+        public bool Constant { get; set; }
+
+        [JsonProperty("inputs")]
+        public List<Input> Inputs { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("outputs")]
+        public List<Output> Outputs { get; set; }
+
+        [JsonProperty("payable")]
+        public bool Payable { get; set; }
+
+        [JsonProperty("stateMutability")]
+        public string StateMutability { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("anonymous")]
+        public bool? Anonymous { get; set; }
+    }
+}
