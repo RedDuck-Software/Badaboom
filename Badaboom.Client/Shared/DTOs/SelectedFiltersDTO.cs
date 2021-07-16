2406d521276a3f0edbc00db0ef2abb26cc8ecee1
kostyamospan <kostya04042001@gmail.com>
2021-07-16 14:35:39 +0300

    DropDown component, selected filter to non static class, method filtering

---
+++ Badaboom.Client/Shared/DTOs/SelectedFiltersDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace Badaboom.Client.Shared.DTOs
+{
+    public class SelectedFiltersDTO
+    {
+        public bool BlockNumber { get; set; }
+        public bool ContractAddress { get; set; }
+        public bool MethodId { get; set; }
+
+        public bool IsAnyFilterSelected
+        {
+            get
+            {
+                if (BlockNumber || ContractAddress || MethodId)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
--- Badaboom.Client/Shared/Filters/Bases/MethodsFilteringBase.cs
+++ Badaboom.Client/Shared/Filters/Bases/MethodsFilteringBase.cs
@@ -1,23 +1,59 @@
-﻿using Badaboom.Core.Models.DTOs;
+﻿using Badaboom.Client.Shared.DTOs;
+using Badaboom.Core.Models.DTOs;
+using Badaboom.Core.Models.Request;
 using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Badaboom.Client.Shared.Filters
 {
     public class MethodsFilteringBase : ComponentBase
     {
         [Parameter]
-        public IEnumerable<Method> ContractMethods { get; set; } 
+        public IEnumerable<Method> ContractMethods { get; set; }
+
+        [Parameter]
+        public GetFilteredTransactionRequest TransactionFilters { get; set; }
+
+        [Parameter]
+        public SelectedFiltersDTO SelectedFilters { get; set; }
+
+        [Parameter]
+        public Pages.Index Index { get; set; }
+
+        public Method SelectedMethod { get; set; }
+
+        public List<Input> SelectedMethodAvailableArguments { get; set; } = new();
+
+        public List<Input> SelectedArguments { get; set; } = new();
 
         [Inject]
         public HttpClient Http { get; set; }
-            
 
-        protected override async Task OnInitializedAsync()
+
+        protected void RemoveSelectedArgument(Input arg)
         {
+            SelectedArguments.Remove(arg);
+            SelectedMethodAvailableArguments.Add(arg);
+            StateHasChanged();
+        }
 
+        protected void OnSelected(Method selection)
+        {
+            SelectedMethod = selection;
+            SelectedMethodAvailableArguments = selection.Inputs.ToList();
+            StateHasChanged();
+        }
+
+        protected void OnArgumentSelected(Input input)
+        {
+            Console.WriteLine(SelectedMethod);
+            SelectedArguments.Add(input);
+            SelectedMethodAvailableArguments.Remove(input);
+            StateHasChanged();
         }
     }
 }
