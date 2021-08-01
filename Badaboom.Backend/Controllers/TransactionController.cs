f2fba5f874936b6743c94c4c043930d6f4b52139
Mark Virchenko <mark.virchenko@redduck.io>
2021-08-01 16:30:29 +0300

    resolve warnings

--- Badaboom.Backend/Controllers/TransactionController.cs
+++ Badaboom.Backend/Controllers/TransactionController.cs
@@ -3,16 +3,8 @@ using Badaboom.Backend.Infrastructure.Services;
 using Badaboom.Core.Models.Enums;
 using Badaboom.Core.Models.Request;
 using Badaboom.Core.Models.Response;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Badaboom.Backend.Attributes;
 
