3008279a38031ff711304870acd6b0ec8c8369ae
AlexandrOkushko <sevenr23@gmail.com>
2021-08-04 14:08:26 +0300

    add pricing page and product component

---
+++ Badaboom.Client/Pages/Pricing/Pricing.razor.cs
@@ -0,0 +1,43 @@
+﻿using Badaboom.Core.Models.Enums;
+using Badaboom.Core.Models.Response;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Badaboom.Client.Pages.Pricing
+{
+    public partial class Pricing
+    {
+        private bool loading = true;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        public List<(ProductType Product, ProductPriceResponse ProductPrice)> ProductsPrice { get; set; } = new();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            foreach (ProductType product in Enum.GetValues<ProductType>())
+            {
+                var httpResponse = await Http.GetAsync($"/api/Payment/productPrice?ProductType={product}");
+
+                var responseString = await httpResponse.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseString);
+
+                ProductsPrice.Add((product, JsonSerializer.Deserialize<ProductPriceResponse>(responseString,
+                    new JsonSerializerOptions() { PropertyNameCaseInsensitive = true })));
+                ProductsPrice.Add((product, JsonSerializer.Deserialize<ProductPriceResponse>(responseString,
+                    new JsonSerializerOptions() { PropertyNameCaseInsensitive = true })));
+                ProductsPrice.Add((product, JsonSerializer.Deserialize<ProductPriceResponse>(responseString,
+                    new JsonSerializerOptions() { PropertyNameCaseInsensitive = true })));
+            }
+
+            loading = false;
+        }
+    }
+}
