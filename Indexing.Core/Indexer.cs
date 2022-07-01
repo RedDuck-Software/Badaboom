a90326239a14821b8bf9c17bb8d09d70ddcbba59
Artem <artem.byruko@redduck.io>
2022-07-01 10:54:34 +0200

    worked on the PR comments

--- Indexing.Core/Indexer.cs
+++ Indexing.Core/Indexer.cs
@@ -54,11 +54,7 @@ namespace IndexerCore
                 catch (Exception ex)
                 {
                     Logger.LogCritical($"Super critical exception occured. Exiting the app. Ex: {ex.Message}");
-
-                    if (attempts >= MaxAttempts)
-                        throw ex;
-                    else
-                        System.Threading.Thread.Sleep(DelayAttempts * 1000);
+                    ThrowOrDelay(attempts, ex);
                 }
             }
         }
@@ -107,11 +103,7 @@ namespace IndexerCore
                     catch (Exception ex)
                     {
                         Logger.LogCritical("Error while saving BlockQueue into database. ex: " + ex.Message);
-
-                        if (attempts >= MaxAttempts)
-                            Environment.Exit(1);
-                        else
-                            System.Threading.Thread.Sleep(DelayAttempts * 1000);
+                        ThrowOrDelay(attempts, ex);
                     }
                 }
 
@@ -179,11 +171,7 @@ namespace IndexerCore
                 catch (Exception ex) // if exception wasn`t handled in IndexBlock method - this is a Rpc call Exception
                 {
                     Logger.LogCritical($"Super critical exception occured. Exiting the app. Ex: {ex.Message}");
-                    
-                    if (attempts >= MaxAttempts)
-                        Environment.Exit(1);
-                    else
-                        System.Threading.Thread.Sleep(DelayAttempts * 1000);
+                    ThrowOrDelay(attempts, ex);
                 }
 
                 break;
@@ -233,11 +221,7 @@ namespace IndexerCore
                 catch (Exception ex)
                 {
                     Logger.LogError($"GetBlockTransactions() Failed on block {blockNubmer}. Ex: {ex.Message}");
-                    
-                    if (attempts >= MaxAttempts)
-                        throw ex;
-                    else
-                        System.Threading.Thread.Sleep(DelayAttempts * 1000);
+                    ThrowOrDelay(attempts, ex);
                 }
 
                 break;
@@ -351,6 +335,14 @@ namespace IndexerCore
             return value.Substring(0, value.Length > 10 ? 10 : value.Length);
         }
 
+        private async void ThrowOrDelay(int attempts, Exception ex)
+        {
+            if (attempts >= MaxAttempts)
+                throw ex;
+            else
+                await Task.Delay(DelayAttempts * 1000);
+        }
+        
         private readonly string _rpcUrl;
 
         private readonly IWeb3Tracer _web3Tracer;
